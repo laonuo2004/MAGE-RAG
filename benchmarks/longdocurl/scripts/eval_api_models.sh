0dddc37c319950ff75d191383bf5b48ea489a764
legendundery <3763524986@qq.com>
2026-04-22 16:49:39 +0800

    update

--- benchmarks/longdocurl/scripts/eval_api_models.sh
+++ benchmarks/longdocurl/scripts/eval_api_models.sh
@@ -1,5 +1,5 @@
 python eval/api_models/eval_api_models.py \
     --qa_file data/LongDocURL.jsonl \
-    --process_mode serial \
+    --process_mode parallel \
     --image_prefix /root/autodl-tmp/ylz/NeurIPS_2026/code/benchmarks/longdocurl/data/pdf_pngs/4000-4999 \
-    --model_name google/gemma-4-26b-a4b-it:free
+    --model_name google/gemma-3-27b-it
